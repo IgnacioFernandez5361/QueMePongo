91bf1ebbfc3ae0698104488f0cee53cebbe2934d
Tobias Castañeda <tobias.2000david@gmail.com>
2022-04-27 18:42:27 -0300

    Empiezo requerimiento especificar aspectos secundario (colores, materiales, tramas, etc) que esten habilitados. Falta

--- src/src/main/java/qmp1/Prenda/Prenda.java
+++ src/src/main/java/qmp1/Prenda/Prenda.java
@@ -1,7 +1,5 @@
 package qmp1.Prenda;
 
-import qmp1.Prenda.Categorias.CategoriaPrenda;
-
 
 public class Prenda {
     public Color colorPrimario;
@@ -41,6 +39,46 @@ public class Prenda {
 
     public Prenda(TipoPrenda tipoPrenda){
         this.tipoPrenda = tipoPrenda;
+    }
+
+    /*Como usuarie de QuéMePongo, quiero crear una prenda especificando en segundo lugar los aspectos relacionados
+    a su material (colores, material, trama, etc) para evitar elegir materiales inconsistentes con el tipo de prenda.
+     */
+
+    void especificarMateriales(Color colorPrimario, Material material, Trama trama){
+        this.agregarColor(colorPrimario);
+        this.agregarMaterial(material);
+        this.agregarTrama(trama);
+
+        /*Dudas:
+        * Como diseñar esto para Color secundario sabiendo que era opccional? (Podia o no estar)
+        * Tambien la categoria vendria a ser parte de los materiales? (En caso que si, podria usar
+        el metodo coincideCategoria en esta parte) Recordar que aun no estaria definida.
+        *
+        * */
+    }
+
+    private void agregarTrama(Trama trama) {
+        if(tipoPrenda.tramaHabilitada(trama)){
+            this.trama = trama;
+        }
+        /*Caso contrario, a parte de no hacer nada lanza una excepccion*/
+    }
+
+    private void agregarMaterial(Material material) {
+        if(tipoPrenda.materialHabilitado(material)){
+            this.material = material;
+        }
+
+        /*Caso contrario, a parte de no hacer nada lanza una excepccion*/
+    }
+
+    private void agregarColor(Color colorPrimario) {
+        if(tipoPrenda.colorHabilitado(colorPrimario)){
+            this.colorPrimario = colorPrimario;
+        }
+
+        /*Caso contrario, a parte de no hacer nada lanza una excepccion*/
     }
 
 
--- src/src/main/java/qmp1/Prenda/TipoPrenda.java
+++ src/src/main/java/qmp1/Prenda/TipoPrenda.java
@@ -1,7 +1,12 @@
 package qmp1.Prenda;
 
+import java.util.LinkedList;
+
 public class TipoPrenda {
     Categoria categoria;
+    LinkedList<Color> coloresPermitidos;
+    LinkedList<Material> materialesPermitidos;
+    LinkedList<Trama> tramasPermitidas;
 
     TipoPrenda(Categoria categoria){
         this.categoria = categoria;
@@ -11,4 +16,15 @@ public class TipoPrenda {
         return this.categoria == unaCategoria;
     }
 
+    public boolean colorHabilitado(Color colorPrimario) {
+        return coloresPermitidos.contains(colorPrimario);
+    }
+
+    public boolean materialHabilitado(Material material) {
+        return materialesPermitidos.contains((material));
+    }
+
+    public boolean tramaHabilitada(Trama trama) {
+        return tramasPermitidas.contains(trama);
+    }
 }
