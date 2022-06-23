5a2f731dd1124df5f5bf8485baac3c884b47e8f9
lgmezabal <66521111+lgmezabal@users.noreply.github.com>
2022-06-22 22:53:58 -0300

    Agrego clase alertas

--- src/src/main/java/qmp/Meteorologia/Alertas.java
+++ src/src/main/java/qmp/Meteorologia/Alertas.java
@@ -1,1 +1,13 @@
+package qmp.Alertas;
 
+import qmp.Meteorologia.AccuWeatherAPI;
+
+public class Alertas{
+  private AccuWeatherAPI api = new AccuWeatherAPI();
+  private String ciudad;
+  
+  public Map<String, Object> getAlertas{
+    return this.api.getAlerts(ciudad);
+    }
+
+}
