39740f267f8005359e1b6998d90a6e38a7cdbe3c
lgmezabal <66521111+lgmezabal@users.noreply.github.com>
2022-06-22 23:04:14 -0300

    Update Alertas.java

--- src/src/main/java/qmp/Meteorologia/Alertas.java
+++ src/src/main/java/qmp/Meteorologia/Alertas.java
@@ -5,9 +5,16 @@ import qmp.Meteorologia.AccuWeatherAPI;
 public class Alertas{
   private AccuWeatherAPI api = new AccuWeatherAPI();
   private String ciudad;
+  private Map<String, Object> ultimasAlertas;
   
   public Map<String, Object> getAlertas{
-    return this.api.getAlerts(ciudad);
+    ultimasAlertas = this.api.getsAlerts(ciudad);
+    return ultimasAlertas
     }
+  public string getNotificaciones{
+    /* if ultimasAlertas.contiene ("Storm") --> return "Llevar Paraguas"
+    else if ultimasAlertas.contiene("Hail") ----> return "Evita salir en auto"*/
+    return "falta terminar";
+  }
 
 }
